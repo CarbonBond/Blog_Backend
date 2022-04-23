14455c4091cb1a8ab152f09dfef004908f982880
Carbon <Cheeseburgerbbgun@gmail.com>
2022-04-23 15:09:17 -0600

    Added prisma connection, along with a basic /api/version/1/users get call that returns all users

---
+++ routes/version/1.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+/* GET users listing. */
+router.get('/users', async function(req, res, next) {
+  
+  const allUsers = await prisma.user.findMany({
+    select: {
+      name: true,
+      posts: true
+    }
+  })
+
+  res.json(allUsers)
+});
+
+module.exports = router;
